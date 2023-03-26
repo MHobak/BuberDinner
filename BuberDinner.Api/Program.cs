679afa25d603ee360331485f935846bc1db8e63c
Miguel Angel Hobak koh <Maikol_9999@hotmail.com.ar>
2023-03-26 13:15:42 -0600

    Custom problem details factory implementation

--- BuberDinner.Api/Program.cs
+++ BuberDinner.Api/Program.cs
@@ -1,6 +1,7 @@
-using BuberDinner.Api.Filters;
+using BuberDinner.Api.Errors;
 using BuberDinner.Application;
 using BuberDinner.Infrastructure;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 {
@@ -8,14 +9,19 @@ var builder = WebApplication.CreateBuilder(args);
     .AddApplication()
     .AddInfrastructure(builder.Configuration);
 
-    builder.Services.AddControllers(
-        options => options.Filters.Add<ErrorHandlingFilterAttribute>() //add error handling filter to all controllers
-    );
+    // builder.Services.AddControllers(
+    //     options => options.Filters.Add<ErrorHandlingFilterAttribute>() //add error handling filter to all controllers
+    // );
+
+    builder.Services.AddControllers();
+    //Override the default 
+    builder.Services.AddSingleton<ProblemDetailsFactory, BuberDinnerProblemDetailsFactory>();
 }
 
 var app = builder.Build();
 {
     //app.UseMiddleware<ErrorHandlingMiddleware>();
+    app.UseExceptionHandler("/errors"); //Middleware that re-executes the request to the error path
     app.UseHttpsRedirection();
     app.MapControllers();
     app.Run();
